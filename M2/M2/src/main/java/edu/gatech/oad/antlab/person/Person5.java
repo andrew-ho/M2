257d0711d1dc2a8f00621ffb1356b7ce12ad38a2
achan197 <alexc197@gmail.com>
2017-06-01 16:56:12 -0400

    Person5 commit

--- M2/M2/src/main/java/edu/gatech/oad/antlab/person/Person5.java
+++ M2/M2/src/main/java/edu/gatech/oad/antlab/person/Person5.java
@@ -5,7 +5,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  modified string 
  *  
- *  @author Bob
+ *  @author Alexander Chan
  *  @version 1.1
  */
 public class Person5 {
@@ -30,8 +30,11 @@ public class Person5 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 5 put your implementation here
-	  return null;
+		String twoChar;
+		twoChar = input.substring(0,2);
+		input = input.substring(2);
+		input = input + twoChar;
+		return input;
 	}
 	
 	/**
